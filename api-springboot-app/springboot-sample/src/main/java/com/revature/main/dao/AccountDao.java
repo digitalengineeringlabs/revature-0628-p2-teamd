9fdaf94dca352d610486cc69870e6d04ddb813b6
johnm0724 <36236210+johnm0724@users.noreply.github.com>
2021-08-22 12:38:54 -0400

    Add files via upload

---
+++ api-springboot-app/springboot-sample/src/main/java/com/revature/main/dao/AccountDao.java
@@ -0,0 +1,11 @@
+package com.revature.main.dao;
+
+import java.util.List;
+
+import com.revature.main.model.Account;
+
+public interface AccountDao {
+	
+	public List<Account> findAll();
+
+}
---
+++ api-springboot-app/springboot-sample/src/main/java/com/revature/main/manager/AccountManager.java
@@ -0,0 +1,11 @@
+package com.revature.main.manager;
+
+import java.util.List;
+
+import com.revature.main.model.Account;
+
+public interface AccountManager {
+	
+	public List<Account> findAll();
+
+}
