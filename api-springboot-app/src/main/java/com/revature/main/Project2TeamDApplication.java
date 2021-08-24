0d5e6954e5162f233f97b2ee19852d3d8507c88b
Averonda <rearden@live.com>
2021-08-23 19:00:57 -0600

    merge conflicts sorted

---
+++ api-springboot-app/src/main/java/com/revature/main/Project2TeamDApplication.java
@@ -0,0 +1,14 @@
+package com.revature.main;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Project2TeamDApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Project2TeamDApplication.class, args);
+		//System.out.println("Hello World");
+	}
+
+}
